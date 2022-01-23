918327f44046c8103415ea287d27a09673d6ef41
Aden <adenchuayouliang@gmail.com>
2022-01-23 17:48:32 +0800

    updated versioning

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,6 +2,6 @@ export const API_ENDPOINT = "/api";
 
 export const UMS_API_ENDPOINT = "http://localhost:5100/api";
 
-export const APP_VERSION = "1.0.0-a";
+export const APP_VERSION = "1.1.0-a";
 
 export const DRAWER_WIDTH = 240;
